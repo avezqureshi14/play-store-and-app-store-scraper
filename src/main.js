f708e781f8f594db97348871da6b11f106130e9e
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-05 15:31:09 +0530

    fix: created an interface for google play and app store

--- src/main.js
+++ src/main.js
@@ -1,18 +1,110 @@
+import store from "app-store-scraper";
 import gplay from "google-play-scraper";
 import { Actor } from "apify";
+import { category } from "./constants/category.js";
+
+class AppStore {
+  static async listApps({ appStoreCategory, limit, priceModel }) {
+    const selectedCategory = category[appStoreCategory];
+    try {
+      const allApps = await store.list({
+        category: selectedCategory,
+        num: limit
+      });
+
+      let filteredApps = allApps;
+
+      if (priceModel === "FREE") {
+        filteredApps = filteredApps.filter((app) => app.free === true);
+      } else if (priceModel === "PAID") {
+        filteredApps = filteredApps.filter((app) => app.free === false);
+      }
+
+      await Actor.pushData(filteredApps.slice(0, limit));
+    } catch (error) {
+      console.error("Error fetching data from App Store:", error);
+      await Actor.pushData({ error: error.message });
+    }
+  }
+
+  static async listDeveloperApps({ devId }) {
+    try {
+      const apps = await store.developer({ devId: devId });
+      await Actor.pushData(apps);
+    } catch (error) {
+      console.error("Error fetching data from App Store:", error);
+      await Actor.pushData({ error: error.message });
+    }
+  }
+
+  static async getAppDetails({ appId }) {
+    try {
+      const result = await store.app({ appId: appId });
+      await Actor.pushData(result);
+    } catch (error) {
+      console.error("Error fetching data from App Store:", error);
+      await Actor.pushData({ error: "Internal Server Error" });
+    }
+  }
+}
+
+class GooglePlay {
+  static async listApps({ playStoreCategory, playStorePopularity, limit, priceModel }) {
+    try {
+      const allApps = await gplay.list({
+        category: playStoreCategory,
+        collection: playStorePopularity,
+        num: limit,
+      });
+
+      let filteredApps = allApps;
+
+      if (priceModel === "FREE") {
+        filteredApps = filteredApps.filter((app) => app.free === true);
+      } else if (priceModel === "PAID") {
+        filteredApps = filteredApps.filter((app) => app.free === false);
+      }
+
+      await Actor.pushData(filteredApps.slice(0, limit));
+    } catch (error) {
+      console.error("Error fetching data from Google Play:", error);
+      await Actor.pushData({ error: error.message });
+    }
+  }
+
+  static async getAppDetails({ appId }) {
+    try {
+      const result = await gplay.app({ appId: appId });
+      await Actor.pushData(result);
+    } catch (error) {
+      console.error("Error fetching data from Google Play:", error);
+      await Actor.pushData({ error: "Internal Server Error" });
+    }
+  }
+}
 
 const runActor = async () => {
   await Actor.init();
 
   const input = await Actor.getInput();
   const { action } = input;
+
   try {
     switch (action) {
-      case "LIST_APPS":
-        await listApps(input);
+      case "LIST_APP_STORE_APPS":
+        await AppStore.listApps(input);
         break;
-      case "GET_APP_DETAILS":
-        await getAppDetails(input);
+      case "LIST_APP_STORE_DEVELOPER_APPS":
+        await AppStore.listDeveloperApps(input);
+        break;
+      case "GET_APP_STORE_APP_DETAILS":
+        await AppStore.getAppDetails(input);
+        break;
+      case "LIST_GOOGLE_PLAY_APPS":
+        await GooglePlay.listApps(input);
+        break;
+      case "GET_GOOGLE_PLAY_APP_DETAILS":
+        await GooglePlay.getAppDetails(input);
         break;
       default:
         console.error("Invalid action specified in input.");
@@ -25,42 +117,6 @@ const runActor = async () => {
   }
 
   await Actor.exit();
-};
-
-const listApps = async ({ category, popularity, limit, priceModel }) => {
-  try {
-    const allApps = await gplay.list({
-      category: category,
-      collection: popularity,
-      num: limit,
-    });
-
-    let filteredApps = allApps;
-
-    if (priceModel === "FREE") {
-      // Filter apps to include only free apps (Freemium, Ad-Supported, etc.)
-      filteredApps = filteredApps.filter((app) => app.free === true);
-    } else if (priceModel === "PAID") {
-      // Filter apps to include only paid apps
-      filteredApps = filteredApps.filter((app) => app.free === false);
-    }
-
-    await Actor.pushData(filteredApps.slice(0, limit));
-  } catch (error) {
-    console.error("Error fetching data from Google Play:", error);
-    await Actor.pushData({ error: error.message });
-  }
-};
-
-
-const getAppDetails = async ({ appId }) => {
-  try {
-    const result = await gplay.app({ appId: appId });
-    await Actor.pushData(result);
-  } catch (error) {
-    console.error("Error fetching data from Google Play:", error);
-    await Actor.pushData({ error: "Internal Server Error" });
-  }
 };
 
 runActor();
