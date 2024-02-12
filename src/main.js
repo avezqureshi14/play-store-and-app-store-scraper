3a7ddbd447a4ab9a9a4c9d101069c029f3011ac0
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-12 09:24:27 +0530

    fix: naming conventions

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { Actor } from "apify";
-import { GET_DETAILS,  LIST_APPS, LIST_DEVELOPER_APPS} from "./constants/actionTypes.js";
-import { logError } from "./utility/logError.js";
-import { ScraperFactory } from "./scrappers/scrapper-factory.js";
+import { Actor } from 'apify';
+import {
+  GET_DETAILS,
+  LIST_APPS,
+  LIST_DEVELOPER_APPS
+} from './constants/actionTypes.js';
+import { logError } from './utility/logError.js';
+import { ScraperFactory } from './scrappers/scrapper-factory.js';
 
 const runActor = async () => {
   try {
@@ -11,21 +15,26 @@ const runActor = async () => {
     const storeInstance = ScraperFactory.getScraperInstance(platform);
 
     switch (action) {
-      case LIST_APPS:
+      case LIST_APPS: {
         const apps = await storeInstance.listApps(input);
         await Actor.pushData(apps.slice(0, input.limit));
         break;
-      case LIST_DEVELOPER_APPS:
-        await Actor.pushData(await storeInstance.listDeveloperApps(input));
+      }
+      case LIST_DEVELOPER_APPS: {
+        const developerApps = await storeInstance.listDeveloperApps(input);
+        await Actor.pushData(developerApps);
         break;
-      case GET_DETAILS:
-        await Actor.pushData(await storeInstance.getAppDetails(input));
+      }
+      case GET_DETAILS: {
+        const appDetails = await storeInstance.getAppDetails(input);
+        await Actor.pushData(appDetails);
         break;
-      default:
-        await Actor.pushData(
-          logError(new Error("Invalid action specified in input."))
-        );
+      }
+      default: {
+        const errorMessage = 'Invalid action specified in input.';
+        await Actor.pushData(logError(new Error(errorMessage)));
         break;
+      }
     }
   } catch (error) {
     await Actor.pushData(logError(error));
