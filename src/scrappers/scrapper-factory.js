c5b78a2d561307a5e1b7b02a16270c08ab875deb
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-12 09:25:46 +0530

    fix: naming conventions

--- src/scrappers/scrapper-factory.js
+++ src/scrappers/scrapper-factory.js
@@ -1,6 +1,6 @@
-import { APP_STORE, GOOGLE_PLAY } from "../constants/actionTypes.js";
-import { AppStore } from "./app-store/app-store-scrapper.js";
-import { GooglePlayStore } from "./google-play/google-play-scrapper.js";
+import { APP_STORE, GOOGLE_PLAY } from '../constants/actionTypes.js';
+import { AppStore } from './app-store/app-store-scrapper.js';
+import { GooglePlayStore } from './google-play/google-play-scrapper.js';
 
 export class ScraperFactory {
   static appStoreInstance = new AppStore();
@@ -8,12 +8,15 @@ export class ScraperFactory {
 
   static getScraperInstance(platform) {
     switch (platform) {
-      case APP_STORE:
+      case APP_STORE: {
         return ScraperFactory.appStoreInstance;
-      case GOOGLE_PLAY:
+      }
+      case GOOGLE_PLAY: {
         return ScraperFactory.googlePlayStoreInstance;
-      default:
-        throw new Error("Invalid platform");
+      }
+      default: {
+        throw new Error('Invalid platform');
+      }
     }
   }
 }
