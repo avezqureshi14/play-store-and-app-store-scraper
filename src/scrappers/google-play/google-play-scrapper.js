77b85f09db839acee14f9c34e2a690af65a31c8b
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-29 13:10:56 +0530

    fix: controller errors

--- src/scrappers/google-play/google-play-scrapper.js
+++ src/scrappers/google-play/google-play-scrapper.js
@@ -1,18 +1,23 @@
-import { memoized as m } from "google-play-scraper";
-import { ScraperInterface } from "../scrapper-interface.js";
-import { countries } from "../../constants/countries.js";
-import { gplayCategory } from "./constants/category.js";
+import memoize from 'memoizee';
+import { ScraperInterface } from '../scrapper-interface.js';
+import { countries } from '../../constants/countries.js';
+import { gplayCategory } from './constants/category.js';
+import gplay from 'google-play-scraper';
+import { gplaySort } from '../google-play/constants/sort.js';
 
 export class GooglePlayStore extends ScraperInterface {
   constructor() {
     super();
-    
-    // Create memoized versions of functions with default options
-    this.memoizedListApps = m();
-    this.memoizedGetAppDetails = m();
+
+    // Iterate through class methods and memoize them
+    Object.getOwnPropertyNames(GooglePlayStore.prototype).forEach((methodName) => {
+      if (typeof this[methodName] === 'function' && methodName !== 'constructor') {
+        this[methodName] = memoize(this[methodName].bind(this), { promise: true });
+      }
+    });
   }
 
-  async listApps({
+  listApps({
     selectedCollection,
     selectedCategory,
     selectedCountry,
@@ -23,19 +28,19 @@ export class GooglePlayStore extends ScraperInterface {
     const playStoreCountry = countries[selectedCountry];
     const sort = gplaySort[selectedSort];
 
-    return await this.memoizedListApps({
+    return gplay.list({
       category: playStoreCategory,
       collection: playStoreCollection,
       country: playStoreCountry,
-      sort,
+      sort: sort,
     });
   }
 
-  async listDeveloperApps({ devId }) {
-    throw new Error("This parameter only works for App Store");
+  listDeveloperApps({ devId }) {
+    throw new Error('This parameter only works for App Store');
   }
 
-  async getAppDetails({ appId }) {
-    return await this.memoizedGetAppDetails({ appId });
+  getAppDetails({ appId }) {
+    return gplay.app({ appId });
   }
 }
